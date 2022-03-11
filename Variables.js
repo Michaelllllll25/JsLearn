cd9ca1aa33b5b720c08ecb0b73363e7e7dbb44ae
Michael <90289974+Michaelllllll25@users.noreply.github.com>
2022-03-11 17:19:05 -0500

    Update and rename Variables to Variables.js

--- Variables.js
+++ Variables.js
@@ -13,3 +13,16 @@ let humanDogRatio = 7
 let myDogAge = myAge * humanDogRatio
 
 console.log(myDogAge)
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Log the value after each step
+
+let bounsPoints = 50
+console.log(bounsPoints)
+bounsPoints = bounsPoints + 50
+console.log(bounsPoints)
+bounsPoints = 25
+console.log(bounsPoints)
+bounsPoints = 70
+console.log(bounsPoints)

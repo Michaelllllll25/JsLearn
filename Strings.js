e50e0d9f5d3d04e9bcfabfc5bad39f31f391ce74
Michael <90289974+Michaelllllll25@users.noreply.github.com>
2022-03-11 19:19:11 -0500

    Create Strings.js

---
+++ Strings.js
@@ -0,0 +1,27 @@
+// strings
+let username = 'Mike'
+
+// Create a variable, message, that stores the string: "You have three new notifications"
+
+let message = "You have three new notifications"
+console.log(message + ", " + username)
+
+// Create a variable. messageToUser, that contains the message we have logged
+let messageToUser = message + ", " + username
+console.log(messageToUser)
+
+// Create two variables, name and greeting. The name variable should store your name,
+// and the greeting should storee e.g. "Hi, my name is"
+// Create a third variable, myGreeting, that contatenates the two strings
+// Log myGreeting to the console
+let name = "Michael"
+let greeting = "Hi, my name is "
+let myGreeting = greeting + name
+console.log(myGreeting)
+
+
+let points = 4
+let bonusPoints = "10"
+let totalPoints = points + bonusPoints
+console.log(totalPoints)                    // 410
+
